947a55fdace2e3640997d97f51fda990cc672efd
FrancoCanD <f.cancinodiaz@gmail.com>
2026-04-21 07:12:00 -0300

    Creación de package model y controller, con clase Estudiante y EstudianteServlet respectivamente

--- src/main/java/cl/kibernum/miprimerjee/controller/EstudianteServlet.java
+++ src/main/java/cl/kibernum/miprimerjee/controller/EstudianteServlet.java
@@ -1,4 +1,24 @@
 package cl.kibernum.miprimerjee.controller;
 
-public class EstudianteServlet {
+import cl.kibernum.miprimerjee.model.Estudiante;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/estudianteServlet")
+public class EstudianteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Estudiante> estudiantes = new ArrayList<>();
+
+        request.setAttribute("listaestudiantes", estudiantes);
+
+        request.getRequestDispatcher("/estudianteingresado.jsp").forward(request, response);
+    }
 }
